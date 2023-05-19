fd403f635a77f79f7b7e3bdd074110ecfcd7dea8
Evgeniy <mazhukin.evgeniy@gmail.com>
2023-05-19 10:11:59 +0200

    Add base item model (#3)

---
+++ src/d100/swing-app/src/main/java/org/example/listmodels/ItemListModel.kt
@@ -0,0 +1,32 @@
+package org.example.listmodels
+
+import org.example.tables.Collections
+import org.example.tables.DbAccessor
+import org.example.tables.Items
+import javax.swing.event.ListDataListener
+
+class ItemListModel(private val collectionId: Long) : InMemoryModelBase<Items>(
+    object : QueryRunner<Items> {
+        private val queries = DbAccessor.database.itemQueries
+
+        override fun insert(value: String) {
+            queries.insert(collectionId, value)
+        }
+
+        override fun selectAll(): List<Items> {
+            return queries.selectByCollectionId(collectionId).executeAsList()
+        }
+
+        override fun selectSince(id: Long): List<Items> {
+            return queries.selectByCollectionIdSinceItemId(collectionId, id).executeAsList()
+        }
+    }
+) {
+    override fun toString(item: Items): String {
+        return item.name
+    }
+
+    override fun toId(item: Items): Long {
+        return item.itemID
+    }
+}
